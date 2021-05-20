47bd8480ae5ee1813d8f8bd75c82d513d31496f0
‘Shelby <sls1881@gmail.com>
2021-05-20 11:08:35 -0700

    updated list page

--- src/components/app/characters/CharacterList.jsx
+++ src/components/app/characters/CharacterList.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import CharacterItem from './CharacterItem'
 import styles from '../App.css'
+import useCharacters from '../../../hooks/characters'
 
-function CharacterList({ characters }) {
+function CharacterList() {
+
+    const {characters } = useCharacters();
     
     return (
         <div className={styles.characterStyles}>
@@ -17,17 +20,6 @@ function CharacterList({ characters }) {
             </ul>
         </div>
     )
-}
-
-CharacterList.propTypes = {
-characters: PropTypes.arrayOf(
-    PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        name: PropTypes.string.isRequired,
-        species: PropTypes.string.isRequired,
-        image: PropTypes.string.isRequired,
-    })
-)
 }
 
 export default CharacterList
